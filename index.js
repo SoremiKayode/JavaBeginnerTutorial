185ad659b2cb096225699cd24c336c57117c7dd6
soremi kayode <banabaz.sk@gmail.com>
2024-01-20 06:51:41 +0100

    first commit of the document

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+ensure express is installed, check the package manager and node 
+modules to see if it is installed, then require express like below
+instatiate it 
+*/
+const express = require("express");
+const app = express();
+
+// html content to send to user
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is my new express App</title>
+</head>
+<body>
+<h1>Hello world!</h1>  
+</body>
+</html>
+`
+
+// create a get request
+app.get("/", (req, res) => {
+// specify the content header to be html
+res.setHeader('Content-Type', 'text/html');
+// send the html content
+res.send(htmlContent);
+})
+
+// instantiate the server and make it liosten on port 3000
+// you can access it in your browser at http://localhost:3000
+app.listen(3000, () => {
+console.log("Server is running at http://localhost:3000");
+});
